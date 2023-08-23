ffc7d444336cf0f2952f11932833fb34b4b5b32e
Arie <arie@arie.software>
2023-08-23 20:39:46 +0300

    login and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CustomerService } from 'src/customer/customer.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { RefreshTokenService } from 'src/refresh-token/refresh-token.service';
+import { LoginOutput } from 'src/auth/dto/login.output';
 
 @Injectable()
 export class AuthService {
@@ -12,8 +18,13 @@ export class AuthService {
     private refreshTokenService: RefreshTokenService,
   ) {}
 
-  async signIn(email: string, password: string): Promise<any> {
+  async login(email: string, password: string): Promise<LoginOutput> {
     const customer = await this.customerService.findOne({ where: { email } });
+
+    // Might want to throw UnauthorizedException instead, for security purposes
+    if (!customer) {
+      throw new NotFoundException('Customer not found');
+    }
 
     if (!(await bcrypt.compare(password, customer?.hashedPassword))) {
       throw new UnauthorizedException();
@@ -30,5 +41,26 @@ export class AuthService {
       accessToken,
       refreshToken,
     };
+  }
+
+  /**
+   * Registers a new customer to the system.
+   * @param email
+   * @param password
+   * Returns access and refresh tokens.
+   */
+  async signUp(email: string, password: string): Promise<LoginOutput> {
+    if (await this.customerService.findOne({ where: { email } })) {
+      throw new ConflictException('Customer already exists');
+    }
+
+    const customerEntity = await this.customerService.create({
+      email,
+      password,
+    });
+
+    if (customerEntity) {
+      return this.login(email, password);
+    }
   }
 }
